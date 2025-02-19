d79093bac3c23cb5d1f15680dad0bc18cbe60919
sabilaowugui <110769028+sabilaowugui@users.noreply.github.com>
2025-02-19 17:10:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,18 +22,18 @@ var KEY = "S6qE0xJR1cW69c-gy";
 var API = "http://api.seniverse.com/v3/weather/now.json";
 var LOCATION = "长春";
 
- // 获取当前时间戳
+ #获取当前时间戳
   var ts = Math.floor((new Date()).getTime() / 1000);
-  // 构造验证参数字符串
+  #构造验证参数字符串
   var str = "ts=" + ts + "&uid=" + UID;
 
-  // 使用 HMAC-SHA1 方式，以 API 密钥（key）对上一步生成的参数字符串（raw）进行加密
-  // 并将加密结果用 base64 编码，并做一个 urlencode，得到签名 sig
+  #使用 HMAC-SHA1 方式，以 API 密钥（key）对上一步生成的参数字符串（raw）进行加密
+  #并将加密结果用 base64 编码，并做一个 urlencode，得到签名 sig
   var sig = CryptoJS.HmacSHA1(str, KEY).toString(CryptoJS.enc.Base64);
   sig = encodeURIComponent(sig);
   str = str + "&sig=" + sig;
 
-  // 构造最终请求的 url
+ #构造最终请求的 url
   var url = API + "?location=" + LOCATION + "&" + str + "&callback=foo";
 
 def get_weather():
